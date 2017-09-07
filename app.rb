885513d80e2fa8b89eb0b5e6acef5ecdd42cfe93
Nikunj Jain <nikunj.jain@berkeley.edu>
2017-09-07 05:44:51 +0000

    Appfile changed

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
 	get '/' do
-		"<!DOCTYPE html><html><head></head><body><h1>Goodbye World</h1></body></html>"
+		"<!DOCTYPE html><html><head></head><body><h1>Hello Goodbye World</h1></body></html>"
 	end
 end
 
